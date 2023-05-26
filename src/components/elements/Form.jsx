05d895951d97d305b03de1a8929ffe1616a0d67f
wahab <wahabmalik630@gmail.com>
2023-05-26 14:12:45 +0500

    add

--- src/components/elements/Form.jsx
+++ src/components/elements/Form.jsx
@@ -7,41 +7,44 @@ const Form = () => {
       <div className="Form">
         <div className="container-fluid mb-4">
           <div className="row">
-            <div className="col-md-6 mb-3 mb-sm-0 ">
-              <input
-                type="text"
-                placeholder="Full Name"
-                className="form-input text-white"
-              />
-            </div>
-            <div className="col-md-6">
-              <input
-                type="text"
-                placeholder="Email"
-                className="form-input text-white"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="container-fluid mb-4">
-          <div className="row">
-            <div className="col-md-12 ">
-              <textarea
-                className="textareastyle "
-                // maxlength="200"
+            <div className="col-md-12  ">
+              <form
+                action="https://formspree.io/f/xdovvgbw"
+                method="POST"
+                className="contact-inputs"
               >
-                A nice day is a nice day. Lao Tseu
-              </textarea>
+                <div className="d-sm-flex justify-content-between mb-4 ">
+                  <input
+                    type="text"
+                    placeholder="Full Name"
+                    name="fullName"
+                    className="form-input text-white mb-3 mb-sm-0 "
+                    autoComplete="off"
+                    required
+                  />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    className="form-input text-white"
+                    autoComplete="off"
+                    required
+                  />
+                </div>
+                <textarea
+                  className="textareastyle mb-4"
+                  name="textMessage"// ye required elements lazmi likhna he just
+                  autoComplete="off"// bus 
+                  required
+                  // maxlength="200"
+                ></textarea>               
+                <ContactBtn name="Send To Me" type="submit" />
+              </form>
             </div>
           </div>
         </div>
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-12">
-                <ContactBtn name="Send To Me"/>
-                </div>
-            </div>
-        </div>
+
+        
       </div>
     </>
   );
